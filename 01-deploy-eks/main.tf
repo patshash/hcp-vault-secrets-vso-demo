8325474ce2c1f5f7544a94d88fab177df2e9e500
Patrick <82187582+patshash@users.noreply.github.com>
2025-09-30 14:40:33 +1000

    Update k8 ver

--- 01-deploy-eks/main.tf
+++ 01-deploy-eks/main.tf
@@ -55,7 +55,7 @@ module "eks" {
   version = "19.15.3"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.33"
+  cluster_version = "1.32"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
